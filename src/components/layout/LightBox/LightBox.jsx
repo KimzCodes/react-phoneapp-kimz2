7dda007d5094d02f855a83e1f3af5cb43866d931
kareem nour <kimzcodes@gmail.com>
2022-11-23 21:16:48 +0200

    finish

--- src/components/layout/LightBox/LightBox.jsx
+++ src/components/layout/LightBox/LightBox.jsx
@@ -3,26 +3,22 @@ import Container from "../container/Container";
 
 const LightBox = ({ children, closeHandler }) => {
   return (
-    <div className={styles.lightbox} onClick={closeHandler}>
-      <div
+    <>
+      <div className={styles.lightbox} onClick={closeHandler}></div>
+      <Container
         style={{
+          backgroundColor: "white",
+          width: "500px",
           zIndex: 10,
           position: "absolute",
           top: 0,
           left: "50%",
-          transform: "translateX(-50%)",
+          transform: "translate(-50%)",
         }}
       >
-        <Container
-          style={{
-            backgroundColor: "white",
-            width: "500px",
-          }}
-        >
-          {children}
-        </Container>
-      </div>
-    </div>
+        {children}
+      </Container>
+    </>
   );
 };
 
